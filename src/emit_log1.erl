133161d7553d4c02a50a867054de054ce77e88b1
placzkie <leszekplaczkiewicz@gmail.com>
2015-01-23 23:55:10 +0100

    Initial commit

---
+++ src/emit_log1.erl
@@ -0,0 +1,25 @@
+%%#!/usr/bin/env escript
+%%! -pz ./amqp_client ./rabbit_common ./amqp_client/ebin ./rabbit_common/ebin
+-module(emit_log1).
+-include_lib("amqp_client/include/amqp_client.hrl").
+-export([main/1]).
+
+main(Argv) ->
+  {ok, Connection} =
+    amqp_connection:start(#amqp_params_network{host = "localhost"}),
+  {ok, Channel} = amqp_connection:open_channel(Connection),
+
+  amqp_channel:call(Channel, #'exchange.declare'{exchange = <<"logs">>,
+    type = <<"fanout">>}),
+
+  Message = case Argv of
+              [] -> <<"info: Hello World!">>;
+              Msg -> list_to_binary(Msg)%%string:join(Msg, " "))
+            end,
+  amqp_channel:cast(Channel,
+    #'basic.publish'{exchange = <<"logs">>},
+    #amqp_msg{payload = Message}),
+  io:format(" [x] Sent ~p~n", [Message]),
+  ok = amqp_channel:close(Channel),
+  ok = amqp_connection:close(Connection),
+  ok.
